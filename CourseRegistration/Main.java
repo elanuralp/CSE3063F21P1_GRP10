18c1fc0b3fffc5e85623a73ad3189aee2390ce42
EmirhanMRL <93511474+EmirhanMRL@users.noreply.github.com>
2021-11-02 20:30:58 +0300

    Update Main.java

--- CourseRegistration/Main.java
+++ CourseRegistration/Main.java
@@ -4,5 +4,6 @@ import java.util.ArrayList;
 
 public class Main {
     ArrayList<Course>prerequisite=new ArrayList<Course>();
-    Course ATA101 = new Course("Atatürk", "ATA101",prerequisite,1,2,"FC",0);
+    FC ATA121 =  new FC("Atatürk", "ATA101",prerequisite,1,2,0,0);
+    
 }
