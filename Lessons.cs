5f11f6350a910594734229fe3a165c9ac0e467e5
Faruk <32228542+omermarvel@users.noreply.github.com>
2021-11-05 18:33:03 +0300

    Update Lessons.cs

--- Lessons.cs
+++ Lessons.cs
@@ -8,7 +8,7 @@ public class Main {
     FC MATH1001 =  new FC("Calculus 1", "MATH1001",prerequisite,1,6,0,0);
     FC CHEM1007= new FC("Basic Chemistry", "CHEM1007",prerequisite,1,6,0,0);
     DC CSE1141 = new DC("Computer Programming 1", "CSE1141",prerequisite,1,6,0,0);
-    FC PHYS1101= new FC("Physics 1", "PHYS1103",prerequisite,1,2,0,0);
+    FC PHYS1101= new FC("Physics 1", "PHYS1101",prerequisite,1,2,0,0);
     FC CSE1100= new FC("Introduction to Computer Eng.", "CSE1100",prerequisite,1,2,0,0);
     DC TRD121= new DC("Turkish Language 1", "TRD121",prerequisite,1,2,0,0);
     FC PHYS1103= new FC("Physics Lab 1", "PHYS1103",prerequisite,1,2,0,0);
